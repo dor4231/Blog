efe1c7859e7c1e2024bdea69d85620244c41069c
dor4231 <dor4231@gmail.com>
2018-06-24 11:18:29 +0300

    feat: Detail, Results and Vote views to polls app

--- blog/polls/models.py
+++ blog/polls/models.py
@@ -3,6 +3,7 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -21,3 +22,5 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+
--- blog/polls/views.py
+++ blog/polls/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Welcome to my Blog!")
 
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s" % question_id)
+
+def results(request, question_id):
+    response = "Yourlooking at the results of question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s" % question_id)
