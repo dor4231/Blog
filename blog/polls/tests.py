01d814a08260e4a3d65585dcb7c13d34d05c8c02
dor4231 <dor4231@gmail.com>
2018-06-28 11:35:03 +0300

    test: was_published_recently() all functionality covered

--- blog/polls/tests.py
+++ blog/polls/tests.py
@@ -17,4 +17,22 @@ class QuestionModelTests(TestCase):
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
 
+    def test_was_published_recently_with_old_question(self):
+        """
+         was_published_recently() returns False for questions whose pub_date
+         is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+         was_published_recently()  returns True for question whose pub_date
+         is within the last day.
+        :return:
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), True)
 
