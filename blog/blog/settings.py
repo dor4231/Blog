45558d6c6b7323950115e03a6c15018c0feff214
Dor Bar <dor4231@gmail.com>
2018-06-30 21:41:45 +0300

    Change postgres password

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -76,7 +76,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mydb',
         'USER': 'postgres',
-        'PASSWORD': 'Idomoo20!',
+        'PASSWORD': 'dor4231',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
