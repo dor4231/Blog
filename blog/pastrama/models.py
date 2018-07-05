1b56291473811b5139277dfb1df8a17e682dff1e
dor4231 <dor4231@gmail.com>
2018-07-05 11:09:59 +0300

    chor: Request model

--- blog/pastrama/models.py
+++ blog/pastrama/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Request(models.Model):
+    domain = models.CharField(max_length=100)
+    service = models.CharField(max_length=100)
+    ipr = models.BooleanField()
+    account_id = models.IntegerField()
+    data = models.TextField
+    sent_at = models.DateTimeField(auto_now_add=True)
